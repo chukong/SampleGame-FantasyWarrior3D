2c266b7a7d462b9cbfe8fae7154f56ded557e530
Kirito <zijian.rao@chukong-inc.com>
2014-10-13 14:44:47 +0800

    Bind Water Sprite

---
+++ frameworks/cocos2d-x/cocos/custom/Water.h
@@ -0,0 +1,26 @@
+//
+//  water.h
+//  watertest
+//
+//  Created by WuHao on 14/10/11.
+//
+//
+
+#ifndef __watertest__water__
+#define __watertest__water__
+
+#include "cocos2d.h"
+
+USING_NS_CC;
+
+NS_CC_BEGIN
+
+class CC_DLL Water : public cocos2d::Sprite
+{
+public:
+    static Water * create(const std::string& tex1,const std::string& tex2,const std::string& tex3, const cocos2d::Size& size, float hSpeed, float vSpeed, float saturation);
+};
+
+NS_CC_END
+
+#endif
