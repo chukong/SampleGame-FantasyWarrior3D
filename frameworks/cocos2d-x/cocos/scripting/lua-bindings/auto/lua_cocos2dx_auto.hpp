fd2d06bf906eaf3b52cabf1d750c3a761dec8081
gaohuang <2933682586@qq.com>
2014-10-16 11:29:12 +0800

    binding move and effectsprite and pointlight

--- frameworks/cocos2d-x/cocos/scripting/lua-bindings/auto/lua_cocos2dx_auto.hpp
+++ frameworks/cocos2d-x/cocos/scripting/lua-bindings/auto/lua_cocos2dx_auto.hpp
@@ -1582,4 +1582,36 @@ int register_all_cocos2dx(lua_State* tolua_S);
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #endif // __cocos2dx_h__
--- frameworks/cocos2d-x/cocos/scripting/lua-bindings/auto/lua_cocos2dx_custom.hpp
+++ frameworks/cocos2d-x/cocos/scripting/lua-bindings/auto/lua_cocos2dx_custom.hpp
@@ -130,4 +130,17 @@ int register_all_cocos2dx_custom(lua_State* tolua_S);
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
 #endif // __cocos2dx_custom_h__
