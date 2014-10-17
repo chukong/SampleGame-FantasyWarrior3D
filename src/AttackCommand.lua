661dc528aaa8825bd902da3005127460059b5442
calfjohn <calfjohn@gmail.com>
2014-10-17 18:40:42 +0800

    command

--- src/AttackCommand.lua
+++ src/AttackCommand.lua
@@ -3,43 +3,60 @@ require "Manager"
 
 AttackCommand = 
 class("AttackCommand",function() 
-        return {raceType = 0, position = cc.p(0,0), radius = 0, attack = 0, attackType = {melee = true, AOE = false, range = 0}, valid = true} 
+        return {raceType = 0, position = cc.p(0,0), target = 0, attackType = {attack = 0, attackRadius = 0, melee = true, AOE = false, validDistance = 0}} 
       end)
       
 AttackCommandManager = List.new()
 
 function AttackCommand.create(object)
     local command = AttackCommand.new()
-    command.attacker = object
     command.raceType = object._racetype
     command.position = getPosTable(object)
-    command.radius = object._attackRadius
-    command.attack = object.attack
-    command.attackType.melee = false
+    command.target = object._target
+    command.attackType.attack = object.attack
+    command.attackType.validDistance = 0
+    command.attackType.attackRadius = object._attackRadius
+    command.attackType.melee = true
     command.attackType.AOE = false
-    command.attackType.range = 50
+    command.attackType.validDistance = 0
     List.pushlast(AttackCommandManager, command)
+end
+
+local function isInCircleSector(command, object2)
+    local attackDistance = command.attackRadius + object2._radius
+    local tempDistance = cc.pGetDistance(command.position, object2:getPosition())
+    if tempDistance < attackDistance then
+        return true
+    end 
+
+    return false
+end
+
+function AttackCommand:attack()
+    if self.target ~= nil then 
+        self.target:setState(EnumStateType.KNOCKED)
+    end
 end
 
 function AttackCommand:attackAll()
     if self.raceType ~= EnumRaceType.WARRIOR and self.raceType ~= EnumRaceType.MAGE and self.raceType ~= EnumRaceType.ARCHER then
         for val = HeroManager.first, HeroManager.last do
             local sprite = HeroManager[val]
-            if sprite._isalive == true and isInCircleSector(self.attacker, sprite) then
+            if sprite._isalive == true and isInCircleSector(self, sprite) then
                 sprite:setState(EnumStateType.KNOCKED)
             end
         end
     else
         for val = MonsterManager.first, MonsterManager.last do
             local sprite = MonsterManager[val]
-            if sprite._isalive == true and isInCircleSector(self.attacker, sprite) then
+            if sprite._isalive == true and isInCircleSector(self, sprite) then
                 sprite:setState(EnumStateType.KNOCKED)
             end
         end 
 
         for val = BossManager.first, BossManager.last do
             local sprite = BossManager[val]
-            if sprite._isalive == true and isInCircleSector(self.attacker, sprite) then
+            if sprite._isalive == true and isInCircleSector(self, sprite) then
                 sprite:setState(EnumStateType.KNOCKED)
             end
         end 
@@ -51,7 +68,13 @@ function commandControl()
     
     repeat 
         local command = List.popfirst(AttackCommandManager)
-        if command ~= nil then command:attackAll() end
+        if command ~= nil then 
+            if command.attackType.AOE == true then
+                command:attackAll() 
+            else
+                command:attack() 
+            end
+        end
     until command  == nil
 end
 
