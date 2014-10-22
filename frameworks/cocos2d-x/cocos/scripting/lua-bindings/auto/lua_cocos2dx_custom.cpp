38b82209a674b4b63b38210fc3168a95837a3e8e
gaohuang <2933682586@qq.com>
2014-10-22 16:16:33 +0800

    modify billboardparticlesystme

--- frameworks/cocos2d-x/cocos/scripting/lua-bindings/auto/lua_cocos2dx_custom.cpp
+++ frameworks/cocos2d-x/cocos/scripting/lua-bindings/auto/lua_cocos2dx_custom.cpp
@@ -2513,6 +2513,55 @@ int lua_cocos2dx_custom_BillboardParticleSystem_getStartSize(lua_State* tolua_S)
 
     return 0;
 }
+int lua_cocos2dx_custom_BillboardParticleSystem_setTextureWithRect(lua_State* tolua_S)
+{
+    int argc = 0;
+    cocos2d::BillboardParticleSystem* cobj = nullptr;
+    bool ok  = true;
+
+#if COCOS2D_DEBUG >= 1
+    tolua_Error tolua_err;
+#endif
+
+
+#if COCOS2D_DEBUG >= 1
+    if (!tolua_isusertype(tolua_S,1,"cc.BillboardParticleSystem",0,&tolua_err)) goto tolua_lerror;
+#endif
+
+    cobj = (cocos2d::BillboardParticleSystem*)tolua_tousertype(tolua_S,1,0);
+
+#if COCOS2D_DEBUG >= 1
+    if (!cobj) 
+    {
+        tolua_error(tolua_S,"invalid 'cobj' in function 'lua_cocos2dx_custom_BillboardParticleSystem_setTextureWithRect'", nullptr);
+        return 0;
+    }
+#endif
+
+    argc = lua_gettop(tolua_S)-1;
+    if (argc == 2) 
+    {
+        cocos2d::Texture2D* arg0;
+        cocos2d::Rect arg1;
+
+        ok &= luaval_to_object<cocos2d::Texture2D>(tolua_S, 2, "cc.Texture2D",&arg0);
+
+        ok &= luaval_to_rect(tolua_S, 3, &arg1, "cc.BillboardParticleSystem:setTextureWithRect");
+        if(!ok)
+            return 0;
+        cobj->setTextureWithRect(arg0, arg1);
+        return 0;
+    }
+    CCLOG("%s has wrong number of arguments: %d, was expecting %d \n", "cc.BillboardParticleSystem:setTextureWithRect",argc, 2);
+    return 0;
+
+#if COCOS2D_DEBUG >= 1
+    tolua_lerror:
+    tolua_error(tolua_S,"#ferror in function 'lua_cocos2dx_custom_BillboardParticleSystem_setTextureWithRect'.",&tolua_err);
+#endif
+
+    return 0;
+}
 int lua_cocos2dx_custom_BillboardParticleSystem_setStartSpinVar(lua_State* tolua_S)
 {
     int argc = 0;
@@ -4975,6 +5024,7 @@ int lua_register_cocos2dx_custom_BillboardParticleSystem(lua_State* tolua_S)
         tolua_function(tolua_S,"setEndColorVar",lua_cocos2dx_custom_BillboardParticleSystem_setEndColorVar);
         tolua_function(tolua_S,"getAtlasIndex",lua_cocos2dx_custom_BillboardParticleSystem_getAtlasIndex);
         tolua_function(tolua_S,"getStartSize",lua_cocos2dx_custom_BillboardParticleSystem_getStartSize);
+        tolua_function(tolua_S,"setTextureWithRect",lua_cocos2dx_custom_BillboardParticleSystem_setTextureWithRect);
         tolua_function(tolua_S,"setStartSpinVar",lua_cocos2dx_custom_BillboardParticleSystem_setStartSpinVar);
         tolua_function(tolua_S,"resetSystem",lua_cocos2dx_custom_BillboardParticleSystem_resetSystem);
         tolua_function(tolua_S,"setAtlasIndex",lua_cocos2dx_custom_BillboardParticleSystem_setAtlasIndex);
--- frameworks/cocos2d-x/cocos/scripting/lua-bindings/auto/lua_cocos2dx_custom.hpp
+++ frameworks/cocos2d-x/cocos/scripting/lua-bindings/auto/lua_cocos2dx_custom.hpp
@@ -150,4 +150,5 @@ int register_all_cocos2dx_custom(lua_State* tolua_S);
 
 
 
+
 #endif // __cocos2dx_custom_h__
